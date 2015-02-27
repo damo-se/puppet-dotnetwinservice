c7935478ecfdb12fac109543b6b059a25ab683e1
Rich Siegel <rsiegel@ise.com>
2015-02-27 15:53:33 -0500

    remove trailing whitespace in the provider

--- lib/puppet/provider/dotnetwinservice/dotnetwinservice.rb
+++ lib/puppet/provider/dotnetwinservice/dotnetwinservice.rb
@@ -5,7 +5,7 @@ Puppet::Type.type(:dotnetwinservice).provide(:dotnetwinservice) do
   defaultfor  :operatingsystem => :windows
 
   INSTALLUTIL = "#{ENV['SYSTEMROOT']}\\Microsoft.NET\\Framework\\v4.0.30319\\InstallUtil.exe"
-  
+
   commands :default_installutil => INSTALLUTIL
 
   def installutil(*args)
@@ -13,12 +13,12 @@ Puppet::Type.type(:dotnetwinservice).provide(:dotnetwinservice) do
    frameworkpath = @resource[:sixtyfourbit] ? "Framework64" : "Framework"
 
     if @resource[:dotnetversion]
-      
+
       installutilpath = "#{ENV['SYSTEMROOT']}\\Microsoft.NET\\#{frameworkpath}\\v#{@resource[:dotnetversion]}\\InstallUtil.exe"
       if !File.exists?(installutilpath)
         raise Puppet::Error, "Cannot find installutil.exe at #{installutilpath}"
       end
-      
+
       args.unshift installutilpath
       Puppet.debug("Executing '#{args.inspect}'")
       execute(args, :failonfail => true)
@@ -26,7 +26,7 @@ Puppet::Type.type(:dotnetwinservice).provide(:dotnetwinservice) do
       raise Puppet::Error, "No .Net version specified."
     end
   end
-  
+
   def create
       installutil("/unattended", @resource[:path])
   end
